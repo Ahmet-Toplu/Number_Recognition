c8130e6070b471ee26ffa4fbd009e2d116f52dd7
AlexHofman <alexxhhofman@gmail.com>
2024-10-17 11:20:26 +0100

    new data set

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ class App:
         self.canvas = Canvas(self.window, bg="black", height=self.canvas_height, width=self.canvas_width)
         self.window.attributes('-topmost', True)
         self.ai = AI()
-        self.ai.load_model("0.1")
+        self.ai.load_model("0.2")
 
         # File to store counters
         self.COUNTERS_FILE = './Number_Recognition/counters.json'
--- Recognition.py
+++ Recognition.py
@@ -15,7 +15,7 @@ class AI:
         # unprocessed data, kept for later
         self.raw_test_images, self.raw_test_labels = self.test_images, self.test_labels
 
-        self.model_version = "0.1"
+        self.model_version = "0.2"
 
     # making a window and displaying the last couple
     def mnist_peek(self, rows, cols):
@@ -41,38 +41,25 @@ class AI:
             return "Model path does not exist"
         
     def train(self):
-        # # printing the multidimensional arrays 
-        # # (because we cant show or even imagine an array with 60k x 28 x 28 elements,
-        # #  its just printing the dimensions)
-        # print("tensor:\t\t shape")
-        # print("-"*22)
-        # print("training images:", train_images.shape)
-        # print("training labels:", train_labels.shape)
-        # print("test images:\t", test_images.shape)
-        # print("test labels:\t", test_labels.shape)
-
-        # # to print the pixel values of the test image 0
-        # print(self.test_images[0])
-
-        # # inspect the first image using the matplotlib graphics library
-        # print('label: ', self.test_labels[0])
-        # plt.imshow(self.test_images[0], cmap=plt.cm.binary)
-        # plt.show()
-
 
         self.train_images = self.train_images.reshape((60000, 28 * 28)) # reshape flattens 28 x 28 arrays
-        self.test_images = self.test_images.reshape((10000, 28 * 28))   # to vectors of 784 elements
-
         self.train_images = self.train_images.astype('float32') / 255   # cast as floats
+
+        self.test_images = self.test_images.reshape((10000, 28 * 28))   # to vectors of 784 elements
         self.test_images = self.test_images.astype('float32') / 255     # and rescale to [0, 1]
 
         self.train_labels = tf.keras.utils.to_categorical(self.train_labels) # encode with the nifty `to_categorical` function
         self.test_labels  = tf.keras.utils.to_categorical(self.test_labels)
 
+        # images = [image for image in os.listdir('./Number_Recognition/images') if image.endswith('.png')]
+        # print(images)
+        # self.new_images = tk.keras.preprocessing.image.load_img('./Number_Recognition/images', color_mode='grayscale', target_size=(28, 28))
+
         # an empty model
         self.model = tf.keras.models.Sequential()
 
         # add 3 layers
+        # sigmoid(w1+a1) * (wn +an) +b)
         self.model.add(tf.keras.layers.Input((28 * 28, )))
         self.model.add(tf.keras.layers.Dense(512, activation='relu'))
         self.model.add(tf.keras.layers.Dense(512, activation='relu'))
@@ -93,29 +80,17 @@ class AI:
         if ft_image_path and ft_label is not None:
             print("fine tuning on:", ft_label)
             # img
-            ft_img = tf.keras.preprocessing.image.load_img(ft_image_path, color_mode='grayscale', target_size=(28, 28))
-            ft_img_array = tf.keras.preprocessing.image.img_to_array(ft_img)
-            ft_img_array = ft_img_array.reshape(1, 28 * 28)
-            ft_img_array = ft_img_array.astype('float32') / 255
+            ft_img_array = self.load_custom_images(ft_image_path)
 
             # label
             ft_label_array = tf.keras.utils.to_categorical([ft_label], num_classes=10)
 
-            print(ft_label_array)
-
             # Adjust the learning rate for fine-tuning
             self.model.optimizer.learning_rate.assign(0.0001)
-            # breakpoint()
-            # if isinstance(learning_rate, tf.Variable):
-            #     learning_rate.assign(0.00001)
-            # elif isinstance(learning_rate, tf.Tensor):
-            #     self.model.optimizer.learning_rate = tf.keras.backend.variable(0.00001)
-            # else:
-                # tf.keras.backend.set_value(self.model.optimizer.learning_rate, 0.00001)
-
 
             # train the model again
             self.model.fit(ft_img_array, ft_label_array, epochs=10, batch_size=1)
+            self.save_model(self.model_version)
         else:
             if ft_image_path and ft_label is None:
                 return (False, "No label provided")
@@ -128,10 +103,8 @@ class AI:
 
     def predict(self, image_path = None):
         if image_path:
-            img = tf.keras.preprocessing.image.load_img(image_path, color_mode='grayscale', target_size=(28, 28))
-            img = tf.keras.preprocessing.image.img_to_array(img)
-            img = img.reshape(1, 28 * 28)
-            img = img.astype('float32') / 255
+            img = self.load_custom_images(image_path)
+
             preds = self.model.predict(img, verbose=0)
             argm = np.argmax(preds[0])
             fig, ax = plt.subplots()
@@ -153,7 +126,39 @@ class AI:
             return argm
         else:
             return "No image path provided"
+        
+    def load_custom_images(self, path):
+        image = tf.keras.preprocessing.image.load_img(path, color_mode='grayscale', target_size=(28, 28))
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = image.reshape(1, 28 * 28)
+        image = image.astype('float32') / 255
+
+        return image
+
+    def test(self):
+        new_images_array = [image for image in os.listdir('./Number_Recognition/images') if image.endswith('.bmp')]
+        new_image_labels = [int(image.split('_')[-1].split('.')[0]) for image in new_images_array]
+        self.new_images_array = []
+
+        for image in new_images_array:
+            path = os.path.join('./Number_Recognition/images', image)
+            self.new_image_array = self.load_custom_images(path)
+            self.new_images_array.append(self.new_image_array)
+
+        # Convert the list of image arrays to a NumPy array
+        self.new_images_array = np.vstack(self.new_images_array)
+
+        # One-hot encode the labels
+        self.new_image_labels = tf.keras.utils.to_categorical(new_image_labels, num_classes=10)
+
+        print("New images shape:", self.new_images_array.shape)  # Should be (number of images, 784)
+        print("New labels shape:", self.new_image_labels.shape)  # Should be (number of images, num_classes)
+
+        # Now you can fit the model
+        self.model.fit(self.new_images_array, self.new_image_labels, epochs=10, batch_size=32)
+        self.save_model(self.model_version)
 
 if __name__ == "__main__":
     ai = AI()
-    ai.train()
+    ai.load_model("0.2")
+    ai.test()
